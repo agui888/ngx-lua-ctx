33c57c6b53a5157ecaef325ddeee66a37e3cc67b
chunpu <527653908@qq.com>
2014-08-02 08:17:19 -0700

    add test

--- test/init.sh
+++ test/init.sh
@@ -2,11 +2,14 @@ pkill -9 nginx
 rm -f /usr/local/openresty/nginx/conf/nginx.conf
 cp test/nginx.conf /usr/local/openresty/nginx/conf/
 
-
-#wget --no-check-certificate https://raw.githubusercontent.com/chunpu/Shim/master/shim.lua
+rm -f shim.lua*
+wget --no-check-certificate https://raw.githubusercontent.com/chunpu/Shim/master/shim.lua
 cp *.lua /tmp/
+cp test/test.lua /usr/local/openresty/nginx/test.lua
 
 /usr/local/openresty/nginx/sbin/nginx
 
+curl -s 'localhost/basic?a=1&b=2'  -H "Connection: keep-alive" -H "Cache-Control: no-cache" -H "X-Forwarded-For: 1.1.1.1,2.2.2.2,  3.3.3.3"
 
-
+#[ "$RET" == "test ok" ]
+#echo $RET
