3e9363e10a62839f72938ba4f1a1836ee422e526
chunpu <527653908@qq.com>
2014-08-05 05:31:24 -0700

    fix terrible bug

--- context.lua
+++ context.lua
@@ -1,141 +1,138 @@
 local _ = require 'shim'
-local cache = {}
 
-local ctx
+return function(ctx)
+    local cache = {}
 
-local req = {
-    get_header = function()
-        return ngx.req.get_headers()
-    end,
+    local req = {
+        get_header = function()
+            return ngx.req.get_headers()
+        end,
 
-    get_url = function()
-    end,
+        get_url = function()
+        end,
 
-    get_method = function()
-        return ngx.req.get_method()
-    end,
+        get_method = function()
+            return ngx.req.get_method()
+        end,
 
-    get_path = function()
-    end,
+        get_path = function()
+        end,
 
-    get_query = function()
-        if not cache.query then
-            local qs = ngx.req.get_uri_args()
-            for k, v in pairs(qs) do
-                if type(v) == 'table' then
-                    qs[k] = v[#v]
+        get_query = function()
+            if not cache.query then
+                local qs = ngx.req.get_uri_args()
+                for k, v in pairs(qs) do
+                    if type(v) == 'table' then
+                        qs[k] = v[#v]
+                    end
+                end
+                cache.query = qs
+            end
+            return cache.query
+        end,
+        
+        get_ips = function()
+            local val = ctx.get('X-Forwarded-For')
+            if val then
+                return _.split(val, ' *, *')
+            end
+            return {}
+        end,
+
+        get_ip = function()
+            return ctx.ips[1] or ngx.var.remote_addr
+        end,
+
+        get = function(k)
+            return ctx.header[k]
+        end,
+    }
+
+    local res = {
+        set_type = function(v)
+            ctx.set('Content-Type', v)
+        end,
+
+        get_type = function()
+            return ngx.header['Content-Type']
+        end,
+        --[[
+        set_body = function(x)
+            -- todo, most important and should flag it has sent   
+        end,]]
+
+        set_status = function(v)
+            ngx.status = v
+        end,
+
+        get_status = function()
+            return ngx.status
+        end,
+        
+        redirect = function(...)
+            ngx.redirect(...)
+        end,
+
+        set = function(k, v) -- need to know if has written
+            ngx.header[k] = v -- response, don't confuse
+        end,
+
+        remove = function(k)
+            ngx.header[k] = nil
+        end
+       
+    }
+
+    local proto = {
+        res = res,
+        req = req,
+        event = {},
+
+        throw = function(err)
+            ctx.emit('error', err)
+        end,
+
+        emit = function(ev, msg)
+            local arr = ctx.event[ev]
+            if arr then
+                _._each(arr, function(fn)
+                    fn(msg)
+                end)
+            end
+        end,
+
+        on = function(ev, fn)
+            if type(ev) == 'string' and type(fn) == 'function' then
+                local event = ctx.event
+                if not event[ev] then
+                    event[ev] = {}
+                end
+                local arr = event[ev]
+                if not _.has(arr, fn) then
+                    _.push(arr, fn)
                 end
             end
-            cache.query = qs
+        end,
+    }
+
+    _.extend(proto, req, res)
+
+    local getter = function(ctx, k)
+        local fn = proto['get_' .. k]
+        if type(fn) == 'function' then
+            return fn()
         end
-        return cache.query
-    end,
-    
-    get_ips = function()
-        local val = ctx.get('X-Forwarded-For')
-        if val then
-            return _.split(val, ' *, *')
-        end
-        return {}
-    end,
-
-    get_ip = function()
-        return ctx.ips[1] or ngx.var.remote_addr
-    end,
-
-    get = function(k)
-        return ctx.header[k]
-    end,
-}
-
-local res = {
-    set_type = function(v)
-        ctx.set('Content-Type', v)
-    end,
-
-    get_type = function()
-        return ngx.header['Content-Type']
-    end,
-    --[[
-    set_body = function(x)
-        -- todo, most important and should flag it has sent   
-    end,]]
-
-    set_status = function(v)
-        ngx.status = v
-    end,
-
-    get_status = function()
-        return ngx.status
-    end,
-    
-    redirect = function(...)
-        ngx.redirect(...)
-    end,
-
-    set = function(k, v) -- need to know if has written
-        ngx.header[k] = v -- response, don't confuse
-    end,
-
-    remove = function(k)
-        ngx.header[k] = nil
+        return cache[k]
     end
-   
-}
 
-local proto = {
-    res = res,
-    req = req,
-    event = {},
-
-    throw = function(err)
-        ctx.emit('error', err)
-    end,
-
-    emit = function(ev, msg)
-        local arr = ctx.event[ev]
-        if arr then
-            _._each(arr, function(fn)
-                fn(msg)
-            end)
+    local setter = function(ctx, k, v)
+        local fn = proto['set_' .. k]
+        if type(fn) == 'function' then
+            fn(v)
+        else
+            cache[k] = v -- dirty
         end
-    end,
-
-    on = function(ev, fn)
-        if type(ev) == 'string' and type(fn) == 'function' then
-            local event = ctx.event
-            if not event[ev] then
-                event[ev] = {}
-            end
-            local arr = event[ev]
-            if not _.has(arr, fn) then
-                _.push(arr, fn)
-            end
-        end
-    end,
-}
-
-_.extend(proto, req, res)
-
-local getter = function(ctx, k)
-    local fn = proto['get_' .. k]
-    if type(fn) == 'function' then
-        return fn()
     end
-    return cache[k]
-end
-
-local setter = function(ctx, k, v)
-    local fn = proto['set_' .. k]
-    if type(fn) == 'function' then
-        fn(v)
-    else
-        cache[k] = v -- dirty
-    end
-end
-
-return function(raw)
-    ctx = raw
 
     for k, v in pairs(proto) do
         if _.indexOf(k, '_') ~= 4 then
--- test/init.sh
+++ test/init.sh
@@ -4,11 +4,28 @@ cp test/nginx.conf /usr/local/openresty/nginx/conf/
 
 rm -f shim.lua*
 wget --no-check-certificate https://raw.githubusercontent.com/chunpu/Shim/master/shim.lua
-cp *.lua /tmp/
-cp test/test.lua /usr/local/openresty/nginx/test.lua
+cp *.lua /tmp
+cp test/lib/*.lua /tmp # lib for require
+cp test/*.lua /usr/local/openresty/nginx/ # entrance
+#cp test/*.lua /usr/local/openresty/nginx/
 
 /usr/local/openresty/nginx/sbin/nginx
 
 RET=`curl -s 'localhost/basic?a=1&b=2'  -H "Connection: keep-alive" -H "Cache-Control: no-cache" -H "X-Forwarded-For: 1.1.1.1,2.2.2.2,  3.3.3.3"`
 echo $RET
-[ "$RET" == "test ok" ]
+[ "$RET" == "test ok" ] && echo test round 1 ok
+
+# ctx should be isolated
+RET2=`curl -s 'localhost/basic?a=1&b=2'  -H "Connection: keep-alive" -H "Cache-Control: no-cache" -H "X-Forwarded-For: 1.1.1.1,2.2.2.2,  3.3.3.3"`
+echo $RET2
+[ "$RET2" == "test ok" ] && echo test round 2 ok
+
+# ctx should be isolated in module function
+RET3=`curl -s 'localhost/module'`
+echo $RET3
+[ "$RET2" == "ok" ]
+
+# ctx should be isolated in module function
+RET4=`curl -s 'localhost/module'`
+echo $RET4
+[ "$RET4" == "ok" ]
